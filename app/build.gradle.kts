90aed9e360f44f0d099f82cb3eaaa47d7043d598
Will Li <Will_Li@jabil.com>
2026-06-25 15:30:11 +0800

    chore: update version to 1.2.4 (versionCode 7) to reflect commit history

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tvbox.checker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 7
+        versionName = "1.2.4"
     }
 
     buildTypes {
